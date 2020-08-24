1db366eecd75fc3148e8147c66bf71b396afaee1
Niall <50669114+niallsh@users.noreply.github.com>
2020-08-24 01:01:29 +0100

    Base code commit

---
+++ src/Grite/Entities/SpriteSheet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace Grite.Entities {
+    public class SpriteSheet {
+        private const int MaxFramesPerRow = 10;
+        
+        // Provided image details
+        private readonly int _frameCount;
+        private readonly int _frameWidth;
+        private readonly int _frameHeight;
+        
+        // New Image
+        private readonly Image _image;
+        private int _row;
+        private int _column;
+
+        public SpriteSheet(int frameCount, int frameWidth, int frameHeight) {
+            _frameCount = frameCount;
+            _frameWidth = frameWidth;
+            _frameHeight = frameHeight;
+            
+            CalculateImageSize(out var width, out var height);
+            _image = new Image<Rgba32>(width, height, new Rgba32(0, 0, 0, 0));
+        }
+
+        /// <summary>
+        /// Calculates the image size based on the frame count, width and height of the image.
+        /// </summary>
+        /// <param name="width">The output image width.</param>
+        /// <param name="height">The output image height.</param>
+        private void CalculateImageSize(out int width, out int height) {
+            width = Math.Min(MaxFramesPerRow, _frameCount) * _frameWidth;
+            var rows = (int) Math.Round((float) _frameCount / MaxFramesPerRow, 0, MidpointRounding.AwayFromZero);
+            height = rows * _frameHeight;
+        }
+        
+        /// <summary>
+        /// Adds a new image to the spritesheet.
+        /// </summary>
+        /// <param name="image">The image to add.</param>
+        public void AddImage(Image image) {
+            _image.Mutate(img => {
+                img.DrawImage(image, GetNextPosition(), 1f);
+            });
+        }
+
+        /// <summary>
+        /// Gets where the next image should be located. Then moves the counters.
+        /// </summary>
+        /// <returns>The point where the next image should be added to.</returns>
+        private Point GetNextPosition() {
+            var horizontal = _column * _frameWidth;
+            var vertical = _row * _frameHeight;
+
+            if (++_column >= MaxFramesPerRow) {
+                _column = 0;
+                _row++;
+            }
+
+            return new Point(horizontal, vertical);
+        }
+
+        /// <summary>
+        /// Saves the image to the provided file path.
+        /// </summary>
+        /// <param name="path">Where to save the file.</param>
+        public void Save(string path) => _image.Save(path);
+    }
+}
---
+++ src/Grite/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Grite.Entities;
+using SixLabors.ImageSharp;
+
+namespace Grite {
+    class Program {
+        private const int MaxPerRow = 8;
+        
+        static void Main(string[] args) {
+            if (args.Length == 0)
+                ExitApp("No image provided!");
+            
+            var imagePath = args[0];
+            if (!imagePath.EndsWith(".gif"))
+                ExitApp("Image provided doesn't end in gif!");
+            
+            if (!File.Exists(imagePath))
+                ExitApp($"{imagePath} Doesn't exist!");
+
+            Console.Out.WriteLine($"Loading {imagePath}...");
+            
+            // Load the image
+            var inputImage = Image.Load(imagePath);
+            var frameCount = inputImage.Frames.Count;
+            var frameWidth = inputImage.Frames[0].Width;
+            var frameHeight = inputImage.Frames[0].Height;
+            
+            // Create a new image
+            var spriteSheet = new SpriteSheet(frameCount, frameWidth, frameHeight);
+            for (var i = 0; i < inputImage.Frames.Count; i++)
+                spriteSheet.AddImage(inputImage.Frames.CloneFrame(i));
+            
+            // Validate the path
+            var newPath = imagePath.Substring(0, imagePath.Length - 4) + ".png";
+            if (File.Exists(newPath)) {
+                Console.Out.WriteLine($"There's already a file called {newPath}, press enter to replace.");
+                Console.ReadLine();
+            }
+            
+            // Save the image
+            spriteSheet.Save(newPath);
+            Console.Out.WriteLine($"Image saved as {newPath}");
+        }
+
+        private static void ExitApp(string reason) {
+            Console.Out.WriteLine(reason);
+            Console.Out.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
